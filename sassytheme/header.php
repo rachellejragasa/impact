a431770d4e063099cd62090f0600b88d50327053
Rachelle Ragasa <rachelle.ragasa@whitbread.com>
2018-02-17 14:37:22 +0000

    removing defer from scripts

--- sassytheme/header.php
+++ sassytheme/header.php
@@ -11,11 +11,10 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>">
 
-
 <!-- JS -->
-<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.js" defer></script>
-<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() . '/assets/js/init.js'; ?>" defer></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.js" defer></script>
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.js"></script>
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() . '/assets/js/init.js'; ?>"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.js"></script>
 
 </head>
 
